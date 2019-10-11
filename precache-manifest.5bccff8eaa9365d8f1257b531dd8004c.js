56054fae814dd1491a5d00737c5be34abf84d8e4
Kilpijärvi Minna <s1900557@edu.bc.fi>
2019-10-11 10:09:06 +0300

    Updates

---
+++ precache-manifest.5bccff8eaa9365d8f1257b531dd8004c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e43b754e9f295d195d2892b22b0bbf",
+    "url": "/nopeuspeli/index.html"
+  },
+  {
+    "revision": "23ba1be123b53987b271",
+    "url": "/nopeuspeli/static/css/main.43d3dea4.chunk.css"
+  },
+  {
+    "revision": "209aab1640f4aee3514c",
+    "url": "/nopeuspeli/static/js/2.81ba7633.chunk.js"
+  },
+  {
+    "revision": "23ba1be123b53987b271",
+    "url": "/nopeuspeli/static/js/main.0786d15f.chunk.js"
+  },
+  {
+    "revision": "93cab49afb0ead8423c3",
+    "url": "/nopeuspeli/static/js/runtime~main.c1fcc5ca.js"
+  }
+]);
